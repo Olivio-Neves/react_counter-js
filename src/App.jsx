9fa1a798a531438efb4842a8d13a4045d343cbd0
Olivio Neves <olivioservio@gmail.com>
2025-10-02 14:09:37 -0300

    Solution 02-10

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import './App.scss';
 
 export const App = () => {
   const [count, setCount] = useState(0);
-
   const addOne = () => {
     setCount(n => n + 1);
   };
@@ -24,15 +23,12 @@ export const App = () => {
   return (
     <div className="App">
       <h1 className="App__title">{`Count: ${count}`}</h1>
-
       <button type="button" className="App__add-one" onClick={addOne}>
         Add 1
       </button>
-
       <button type="button" className="App__add-100" onClick={add100}>
         Add 100
       </button>
-
       <button type="button" className="App__increase" onClick={increase}>
         Increase
       </button>
